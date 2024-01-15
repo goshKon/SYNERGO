eba722f960c1dbc9f53aaf03a9ce17804628d897
goshKon <124917537+goshKon@users.noreply.github.com>
2024-01-15 14:59:51 +0300

    Update orange.sh
    del comments #

--- orange.sh
+++ orange.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
-#dhcl=$(grep "/sbin/ifconfig eth0 0.0.0.0 0.0.0.0 | dhclient" /etc/rc.local)
-#dhcl_com="# /sbin/ifconfig eth0 0.0.0.0 0.0.0.0 | dhclient &"
 stat_new=$(systemctl status dnsmasq.service | awk '/Active/{print $2}')
 
 echo "Current status of dnsmasq.service: $stat_new"
 
 if grep -E "^(/sbin/ifconfig eth0 0.0.0.0 0.0.0.0|dhclient)" /etc/rc.local
-#if [ "$dhcl" != "$dhcl_com" ] 
 then
  	/sbin/ifconfig eth0 0.0.0.0 0.0.0.0 | dhclient & >/dev/null 2>&1
 	sleep 10
