3a7e32b2d1f4e0a79137b6259e6581742642374d
goshKon <124917537+goshKon@users.noreply.github.com>
2024-01-16 12:44:39 +0300

    Update orange.sh
    fix sleep ()

--- orange.sh
+++ orange.sh
@@ -11,22 +11,22 @@ echo "Current status of dnsmasq.service: $stat_new"
 if grep -E "^(/sbin/ifconfig eth0 0.0.0.0 0.0.0.0|dhclient)" /etc/rc.local
 then
  	/sbin/ifconfig eth0 0.0.0.0 0.0.0.0 | dhclient & >/dev/null 2>&1
-	sleep 10
-	killall -15 openvpn
 	sleep 5
+	killall -9 openvpn
+	sleep 3
 	/usr/sbin/openvpn --config /etc/openvpn/client.ovpn & >/dev/null 2>&1
 	echo "Restarting DHCP"	
    else
         s1=$(systemctl start dnsmasq.service)
-        sleep 10
+        sleep 3
         echo "Starting dnsmasq.service: $s1"
-        sleep 5
+        sleep 3
         s2=$(systemctl restart dnsmasq.service)
-        sleep 10
+        sleep 5
         echo "Restarting dnsmasq.service: $s2"
-        sleep 5
-        killall -15 openvpn
-        sleep 5
+        sleep 3
+        killall -9 openvpn
+        sleep 3
         /usr/sbin/openvpn --config /etc/openvpn/client.ovpn & >/dev/null 2>&1
         echo "Restarting VPN"		
  fi   
