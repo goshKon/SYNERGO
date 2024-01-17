0b66700d970d4ef7a6991291b159c43832349dca
goshKon <124917537+goshKon@users.noreply.github.com>
2024-01-18 00:48:14 +0300

    Update orange.sh
    full restyling script == rasp

--- orange.sh
+++ orange.sh
@@ -1,22 +1,42 @@
 #!/bin/sh
-# Зеленый цвет
+
+# Голубой цвет
 LIGHT_CYAN="\033[1;36m"
 # Сброс цвета
 NC='\033[0m'
-stat_new=$(systemctl status dnsmasq.service | awk '/Active/{print \$2}')
-echo "Current status of dnsmasq.service: $stat_new"
+fmount.sh
+success=0
 rest_VPN() {
-	killall -9 openvpn
-	sleep 3
-	/usr/sbin/openvpn --config /etc/openvpn/client.ovpn & >/dev/null 2>&1
-	}
+    pkill openvpn
+    sleep 15
+    /usr/sbin/openvpn --config /etc/openvpn/client.ovpn 2>&1 & sleep 15 | while read line
+    do
+        echo $line
+        if echo $line | grep -q "Initialization Sequence Completed"
+        then
+            echo "${LIGHT_CYAN}Tunnel is work! Exiting the script.${NC}"
+            pkill openvpn
+            exit 0
+        fi
+    done &
+
+    wait $!
+}
 
 max_attempts=3
 attempt=0
-while [ $attempt -lt $max_attempts ]
-do
 
-if grep -E "^(/sbin/ifconfig eth0 0.0.0.0 0.0.0.0|dhclient)" /etc/rc.local
+while [ $attempt -lt $max_attempts -a $success -eq 0 ]
+do
+stat_new=$(systemctl status dnsmasq.service | awk '/Active/{print \$2}')
+echo "Current status of dnsmasq.service: $stat_new"
+
+tun0_out=$(/sbin/ifconfig tun0)
+if echo "$tun0_out" | grep -q "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00"
+then
+     echo "${LIGHT_CYAN}Tunnel is work! Exiting the script.${NC}"
+     elif
+grep -E "^(/sbin/ifconfig eth0 0.0.0.0 0.0.0.0|dhclient)" /etc/rc.local
 then
  	/sbin/ifconfig eth0 0.0.0.0 0.0.0.0 | dhclient & >/dev/null 2>&1
 	sleep 5
@@ -35,16 +55,14 @@ else
         echo "Restarting VPN"		
 fi   
 
-tun0_out=$(/sbin/ifconfig tun0)
-if echo "$tun0_out" | grep -q "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00"
-then
-     echo "${LIGHT_CYAN}Tunnel is work! Exiting the script.${NC}"
-     exit 0
-else
-     attempt=$((attempt+1))
-     echo "Attempt $attempt of $max_attempts failed. Retrying..."
-     sleep 10
-fi
+       attempt=$((attempt+1))
+    echo "Attempt $attempt of $max_attempts failed. Retrying..."
+    sleep 10
 done
-echo "Failed to Initialize tunnel after $max_attempts attempts. Exiting script."
-exit 1
+if [ $success -eq 0 ]
+then
+break
+else
+    echo "Failed to Initialize tunnel after $max_attempts attempts. Exiting script."
+    exit 1
+fi
