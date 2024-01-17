2c1aa648991ff5728a2ac1d9d81904849ebfc659
goshKon <124917537+goshKon@users.noreply.github.com>
2024-01-18 00:33:45 +0300

    Update rasp.sh
    restyling full script with attempts and break after Tun work!

--- rasp.sh
+++ rasp.sh
@@ -1,50 +1,61 @@
 #!/bin/sh
+
 # Зеленый цвет
 LIGHT_CYAN="\033[1;36m"
 # Сброс цвета
 NC='\033[0m'
-stat_old=$(systemctl status isc-dhcp-server.service | awk '/Active/{print $2}')
-echo "Current status of isc-dhcp-server.service: $stat_old"
+fmount.sh
+success=0
+rest_VPN() {
+    pkill openvpn
+    sleep 15
+    /usr/sbin/openvpn --config /etc/openvpn/client.ovpn 2>&1 & sleep 15 | while read line
+    do
+        echo $line
+        if echo $line | grep -q "Initialization Sequence Completed"
+        then
+            echo "${LIGHT_CYAN}Tunnel is work! Exiting the script.${NC}"
+            pkill openvpn
+            exit 0
+        fi
+    done &
 
-	rest_VPN() {
-	killall -9 openvpn
-	sleep 3
-	/usr/sbin/openvpn --config /etc/openvpn/client.ovpn & >/dev/null 2>&1
-	}
+    wait $!
+}
 
 max_attempts=3
 attempt=0
-while [ $attempt -lt $max_attempts ]
+
+while [ $attempt -lt $max_attempts -a $success -eq 0 ]
 do
-if [ "$stat_old" = "active" -o "$stat_old" = "inactive" ]
-	then
-           ping -c 5 172.81.0.1 >/dev/null 2>&1
-		if [ $? -eq 0 ] 
-		then
+    stat_old=$(systemctl status isc-dhcp-server.service | awk '/Active/{print \$2}')
+    echo "Current status of isc-dhcp-server.service: $stat_old"
+
+    if [ "$stat_old" = "active" -o "$stat_old" = "inactive" ]
+    then
+        if ping -c 5 172.81.0.1 >/dev/null 2>&1
+        then
             echo "Ping successful"
-	    sleep 3
+            sleep 3
         else
             echo "Rebooting VPN AFTER NO PING"
             sleep 3
-	    rest_VPN
+            rest_VPN
+        fi
+    fi
 
-	fi   
-fi
-#if /sbin/ifconfig tun0 | grep -q "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00"
-tun0_out=$(/sbin/ifconfig tun0)
-if echo "$tun0_out" | grep -q "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00"
-then
- #echo "Initialization Sequence Completed"
- echo "${LIGHT_CYAN}Tunnel is work! Exiting the script.${NC}"
-   exit 0
+    tun0_out=$(/sbin/ifconfig tun0)
+    if echo "$tun0_out" | grep -q "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00"
+    then
+        echo "${LIGHT_CYAN}Tunnel is work! Exiting the script.${NC}"
     elif 
-    grep -E "^(/sbin/ifconfig eth0 0.0.0.0 0.0.0.0|dhclient)" /etc/rc.local
-	then
- /sbin/ifconfig eth0 0.0.0.0 0.0.0.0 | dhclient & >/dev/null 2>&1
+	grep -E "^(/sbin/ifconfig eth0 0.0.0.0 0.0.0.0|dhclient)" /etc/rc.local
+    then
+        /sbin/ifconfig eth0 0.0.0.0 0.0.0.0 && dhclient & >/dev/null 2>&1
         sleep 5
         rest_VPN
         echo "Restarting VPN DHCP"
-else
+    else
         s3=$(systemctl start isc-dhcp-server.service)
         sleep 5
         echo "Starting isc-dhcp-server.service: $s3"
@@ -55,18 +66,16 @@ else
         sleep 3
         rest_VPN
         echo "Restarting VPN fnsh"
-fi   
-attempt=$((attempt+1))
-echo "Attempt $attempt of $max_attempts failed. Retrying..."
-sleep 10
+    fi
+
+    attempt=$((attempt+1))
+    echo "Attempt $attempt of $max_attempts failed. Retrying..."
+    sleep 10
 done
-echo "Failed to Initialize tunnel after $max_attempts attempts. Exiting script."
-exit 1
-    
-# Добавлен код для проверки "Initialization Sequence Completed"
-# if grep -q "Initialization Sequence Completed"
-#if echo "$tun0_out" | grep -q "Initialization Sequence Completed"
-#then
- # echo "${LIGHT_CYAN}Tunnel is work! Exiting the script.${NC}"
-  #exit 0
-#fi
+if [ $success -eq 0 ]
+then
+break
+else
+    echo "Failed to Initialize tunnel after $max_attempts attempts. Exiting script."
+    exit 1
+fi
