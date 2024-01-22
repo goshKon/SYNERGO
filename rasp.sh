20638cbf96d4ae7428d95a5f3ee5967ff8396177
goshKon <124917537+goshKon@users.noreply.github.com>
2024-01-22 21:27:07 +0300

    Update rasp.sh
    restyling full script

--- rasp.sh
+++ rasp.sh
@@ -7,20 +7,21 @@ NC='\033[0m'
 fmount.sh
 success=0
 rest_VPN() {
-    killall -15 openvpn
-    sleep 15
-    /usr/sbin/openvpn --config /etc/openvpn/client.ovpn 2>&1 & sleep 15 | while read line
+    echo "Killing any existing openvpn processes..."
+    killall -9 openvpn
+    sleep 5
+    echo "Starting openvpn..."
+    /usr/sbin/openvpn --config /etc/openvpn/client.ovpn 2>&1 & sleep 5 | while read line
     do
         echo $line
-        #if echo $line | grep -q "Initialization Sequence Completed"
-	if echo $line | grep -Eq "Initialization Sequence Completed|Tunnel is work! Exiting the script." #Использовать, если имеется зацикливание с openvpn
+        if echo $line | grep -Eq "Initialization Sequence Completed|Tunnel is work! Exiting the script."
         then
             echo "${LIGHT_CYAN}Tunnel is work! Exiting the script.${NC}"
-           killall -15 openvpn
+            killall -9 openvpn
+            success=1
             exit 0
         fi
     done &
-
     wait $!
 }
 
@@ -29,7 +30,7 @@ attempt=0
 
 while [ $attempt -lt $max_attempts -a $success -eq 0 ]
 do
-stat_old=$(systemctl status isc-dhcp-server.service | awk '/Active/{print $2}')
+stat_old=$(systemctl status isc-dhcp-server.service | awk '/Active/{print \$2}')
 echo "Current status of isc-dhcp-server.service: $stat_old"
 
     if [ "$stat_old" = "active" -o "$stat_old" = "inactive" ]
@@ -49,9 +50,9 @@ echo "Current status of isc-dhcp-server.service: $stat_old"
     if echo "$tun0_out" | grep -q "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00"
     then
         echo "${LIGHT_CYAN}Tunnel is work! Exiting the script.${NC}"
-	break
-    elif 
-	grep -E "^(/sbin/ifconfig eth0 0.0.0.0 0.0.0.0|dhclient)" /etc/rc.local
+        break
+    elif
+        grep -E "^(/sbin/ifconfig eth0 0.0.0.0 0.0.0.0|dhclient)" /etc/rc.local
     then
         /sbin/ifconfig eth0 0.0.0.0 0.0.0.0 && dhclient & >/dev/null 2>&1
         sleep 5
@@ -71,13 +72,9 @@ echo "Current status of isc-dhcp-server.service: $stat_old"
     fi
 
     attempt=$((attempt+1))
-    echo "Attempt $attempt of $max_attempts failed. Retrying..."
-    sleep 10
+    if [ $success -eq 0 ]
+    then
+        echo "Attempt $attempt of $max_attempts failed. Retrying..."
+        sleep 10
+    fi
 done
-if [ $success -eq 0 ]
-then
-break
-else
-    echo "Failed to Initialize tunnel after $max_attempts attempts. Exiting script."
-    exit 1
-fi
