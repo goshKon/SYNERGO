ff81167a159e889fedc77d62e3b440038dc4595e
goshKon <124917537+goshKon@users.noreply.github.com>
2024-01-24 18:18:57 +0300

    Update start.sh
    ipv4 restyle

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# Зеленый цвет
 LIGHT_CYAN="\033[1;36m"
+YELLOW="\033[1;33m"
+PINK="\033[38;5;211m"
+ORANGE="\033[38;5;208m"
 # Сброс цвета
 NC='\033[0m'
 ro=$(cat /proc/cpuinfo | grep "model name" | awk '{print $7}' | head -n 1) 
@@ -68,27 +70,31 @@ echo "${LIGHT_CYAN}Tunnel is work! Exiting the script.${NC}"
 if grep -E "^(/sbin/ifconfig eth0 0.0.0.0 0.0.0.0|dhclient)" /etc/rc.local >/dev/null 2>&1
 then
     # Проверяем наличие строки "netmask" в выводе команды ifconfig для интерфейса eth0
-    if ifconfig eth0 | grep -q "netmask"
+eth0_info=$(ifconfig eth0 | grep "netmask" | awk '{print $3}')
+
+# Если в выводе нет строки "netmask"...
+if [ -z "$eth0_info" ]
+then
+    echo "${LIGHT_CYAN}IPv4 not found, Restarting...${NC}"
+    if [ "$ro" = 3 ]
     then
-        echo "${LIGHT_CYAN}IPv4 found, Exiting...${NC}"
-    else
-        echo "IPv4 not found, Restarting dhcpcd"
+        echo "${YELLOW}Restarting ${PINK}dhcpcd${NC}"
         systemctl restart dhcpcd
-	sleep 3
+    else 
+        dhclient -r eth0 && dhclient eth0
+        echo "${YELLOW}Restarting ${ORANGE}dhclient${NC}"
+        sleep 3
     fi
 else
-    echo "It's Static, stop process."
+    echo "${LIGHT_CYAN}IPv4 found, Exiting...${NC}"
 fi
-elif
-[ "$ro" = "Orange" ]
-then  
-	echo "Starting orange script"
- 	sleep 3
-	sh /etc/scripts/orange.sh
- 	
-else
-	echo "Starting rasp script"
- 	sleep 3
-	sh /etc/scripts/rasp.sh
-fi	
 
+if [ "$ro" = "${ORANGE}Orange${NC}" ]
+then  
+    echo "Starting orange script"
+    sleep 3
+    sh /etc/scripts/orange.sh
+else
+    echo "Starting rasp script"
+    sleep 3
+    sh /etc/scripts/rasp.sh
