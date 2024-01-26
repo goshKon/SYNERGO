bfb625d1ca3e3e8195425a8aa4aad32ca135823b
goshKon <124917537+goshKon@users.noreply.github.com>
2024-01-26 19:24:14 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ PINK="\033[38;5;211m"
 ORANGE="\033[38;5;208m"
 # Сброс цвета
 NC='\033[0m'
-ro=$(cat /proc/cpuinfo | grep "model name" | awk '{print $7}' | head -n 1) 
+ro=$(cat /proc/cpuinfo | grep "model name" | awk '{print $7}' | head -n 1)
 if [ "$ro" = "5" ]
 then
 ro="${ORANGE}Orange${NC}"
@@ -15,15 +15,15 @@ fi
 
 date_rasp=$(date +"%Y-%m-%d") # дата rasp
 fmount.sh
-echo "The current date $ro: ${date_rasp}" 
+echo "The current date $ro: ${date_rasp}"
 sleep 3
 adb_result_formatted=$(adb shell date +"%Y-%m-%d") # переформатирование даты adb в красивый вид
 adb_result="${adb_result_formatted}"
-echo "The current date ADB: ${adb_result}" 
+echo "The current date ADB: ${adb_result}"
 sleep 3
 
 dev1=$(adb devices | grep "device" | awk '{print $2}' | grep "device") # поиск в команде "device"
-	if [ -z "$dev1" ] # проверка на работоспособность ADB
+if [ -z "$dev1" ] # проверка на работоспособность ADB
 then
 echo "Device ADB no found"
 else
@@ -32,11 +32,11 @@ echo "Device connected"
 # проверка на пинг ADB
 if ! adb shell ping -c 5 8.8.8.8 >/dev/null 2>&1
 then
-echo "Error: Failed to restore ping." # пинг ADB не идет 
+echo "Error: Failed to restore ping" # пинг ADB не идет
 exit 1
 fi
 
-		if [ "$date_rasp" = "$adb_result_formatted" ] # проверка на корректность даты из adb в rasp
+if [ "$date_rasp" = "$adb_result_formatted" ] # проверка на корректность даты из adb в rasp
 then
 echo "Date is successfuly, the script is running."
 sleep 3
@@ -61,8 +61,8 @@ date -s "${formatted_date}"
 # Вывести результат
 echo "Date and time are set to $ro: ${formatted_date}"
 sleep 3
-		fi
-	fi
+fi
+fi
 tun0_out=$(/sbin/ifconfig tun0)
 if echo "$tun0_out" | grep -q "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00"
 then
@@ -76,22 +76,13 @@ eth0_info=$(ifconfig eth0 | grep "netmask" | awk '{print $3}')
 if [ -z "$eth0_info" ]
 then
     echo "${LIGHT_CYAN}IPv4 not found, Restarting...${NC}"
-    if [ "$ro" = 3 ]
-    then
-        echo "${YELLOW}Restarting ${PINK}dhcpcd${NC}"
         systemctl restart dhcpcd
-    else 
-        dhclient -r eth0 && dhclient eth0
-        echo "${YELLOW}Restarting ${ORANGE}dhclient${NC}"
-        sleep 3
-    fi
 else
     echo "${LIGHT_CYAN}IPv4 found, Exiting...${NC}"
 fi
 fi
-elif
- [ "$ro" = "${ORANGE}Orange${NC}" ]
-then  
+elif [ "$ro" = "${ORANGE}Orange${NC}" ]
+then
     echo "Starting orange script"
     sleep 3
     sh /etc/scripts/orange.sh
@@ -99,4 +90,4 @@ else
     echo "Starting rasp script"
     sleep 3
     sh /etc/scripts/rasp.sh
-    fi
+fi
